67f69e97285171f7c6973fd5bb162ab2409420d5
Osvaldo Ochoa <153673366+OsvaldoOchoa30@users.noreply.github.com>
2025-02-13 00:15:40 -0600

    Avance disello

--- evaluacionc1/evaluacionc1/urls.py
+++ evaluacionc1/evaluacionc1/urls.py
@@ -6,7 +6,7 @@ from .view import HomePageView, OrdersListViewPage, OrderViewPage, OrderEditarVi
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('custumer/create', CustumerCrearViewPage.as_view(),name='custumer_create'),
+    path('custumer/create', CustumerCrearViewPage.as_view(),name='custumer_create'), 
     path('custumer/edit/<int:pk>/', CustumerEditarViewPage.as_view(), name='custumer_edit'),
     path('custumer/delete/<int:pk>', CustumerEliminarViewPage.as_view(),name='custumer_delete'),
     path('order/list', OrdersListViewPage.as_view(), name='order_list')
